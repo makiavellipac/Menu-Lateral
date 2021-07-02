b21670906ac69ad2e786784a3fd7513448a592a0
santiago <santiago@LAPTOP-93ND2TI6.localdomain>
2021-07-02 09:29:13 -0500

    Se creo la configuración completa de webpack con html, csss y resct

---
+++ webpack.dev.config.ts
@@ -0,0 +1,61 @@
+import path from "path";
+import webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
+import ESLintPlugin from "eslint-webpack-plugin";
+
+const config = {
+  mode: "development",
+  output: {
+    publicPath: "/",
+  },
+  entry: "./src/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript"
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"],
+      }
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js","jsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html"
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new ForkTsCheckerWebpackPlugin({
+      async: false
+    }),
+    new ESLintPlugin({
+      extensions: ["js", "jsx", "ts", "tsx"]
+    })
+  ],
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "build"),
+    historyApiFallback: true,
+    port: 3000,
+    open: true,
+    hot: true
+  },
+};
+
+export default config;
